cff1e3396e29e87e83e1dd1cbaa1e6fa624b4d28
TMax66 <39553077+TMax66@users.noreply.github.com>
2018-10-25 23:23:51 +0200

    Update fix.elem.r

--- fix.elem.r
+++ fix.elem.r
@@ -3,6 +3,29 @@ trasm.cattle=4.30*10e-07, trasm.sheep=2.67*10e-07,
  trasm.pigs=6.67*10e-07, susce.cattle=15.2, susce.sheep=1, susce.pigs=1#,b1=T,b2=T
  , RBLmin=0, RBLml=13, RBLmax=30,RBSmin=0, RBSml=5, RBSmax=7, RSLmin=0, RSLml=11, RSLmax=15,RSSmin=0, RSSml=6, RSSmax=15)
 {
+
+  
+  trasm.cattle=4.30*10e-07
+  trasm.sheep=2.67*10e-07
+  trasm.pigs=6.67*10e-07
+  susce.cattle=15.2
+  susce.sheep=1
+  susce.pigs=1
+  RBLmin=0
+  RBLml=13
+  RBLmax=30
+  RBSmin=0
+  RBSml=5
+  RBSmax=7
+  RSLmin=0
+  RSLml=11
+  RSLmax=15
+  RSSmin=0
+  RSSml=6
+  RSSmax=15
+  tipo.all.caso=''
+  
+  
 require(Hmisc,quietly=T)
 set.seed(as.numeric(substr(date(),18,19)))
 df<-dati
@@ -28,7 +51,34 @@ ifelse(df$herdtype=='S'&df$totale.animali<=1000,'SS','OTHER')))))
 if(tipo.all.caso==''|tipo.all.caso%nin%df$tipologia)index=trunc(runif(1,1,
 dim(df)[1]))else index=sample(which(df$tipologia==tipo.all.caso),1)
 cod.caso.indice=as.character(df[,'codall'][index])
+
+
+
+
+c1 = c(x=3, y=4)
+euc.dist <- function(x1) sqrt(sum((x1 - c1) ^ 2))
+set.seed(123)
+mydf <- data.frame(x=runif(10), y=runif(10))
+apply(mydf, 1, euc.dist)
+
+
+
+
+
 distanze.indice=sqrt(dsquare(matrice,matrice[index,,drop=F]))/1000 # diviso per 1000
+
+
+
+
+
+
+
+
+
+
+
+
+
 distanze.indice=ifelse(df[,2]==df[,2][index],distanze.indice/100,distanze.indice) # posto 100
 df$latency=ifelse(df$herdtype=="S",rbeta.pert(herds,1,6,14),rbeta.pert(herds,1,4,14))#ifelse(df$herdtype=="S",rnorm(herds,6,.9),rnorm(herds,4,.8))
 df$latency=ifelse(df$codall==cod.caso.indice,-1,df$latency)## 4 nov
